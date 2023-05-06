e1669f024982427bb751a1406829191e35b6bbaf
Kevin Mendoza <soporte.jadecarrental@gmail.com>
2023-05-06 00:31:40 -0500

    Se crean las carpetas organizativas del proyecto

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '¡Hola, mundo!'
+def index():
+    return render_template("index.html")
+
+@app.route('/tours')
+def tours():
+    return render_template("tours.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
